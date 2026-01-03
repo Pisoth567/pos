cfb625ce1c58563c01f087ef56249c925169fb46
Pisoth567 <pisoth887@gmail.com>
2026-01-03 15:15:57 +0700

    Data of Order Dishes

---
+++ src/constants/index.js
@@ -0,0 +1,45 @@
+import ice_latte from '../assets/images/ice-americano.jpg';
+import cocktail from '../assets/images/cocktail.webp';
+import ice_americano from '../assets/images/ice-americano.jpg';
+import kafe_dosko from "../assets/images/kafe-dos-ko.jpg";
+import macha from '../assets/images/macha.webp';
+import spar from '../assets/images/Sparkling-Drinks-3.webp';
+
+export const popurlarDishes = [
+  {
+    id: 1,
+    image: ice_latte,
+    name: "Ice Latte",
+    numberOfOrders: 200,
+  },
+  {
+    id: 2,
+    image: ice_americano,
+    name: "Ice Americano",
+    numberOfOrders: 180,
+  },
+  {
+    id: 3,
+    image: macha,
+    name: "Matcha Latte",
+    numberOfOrders: 150,
+  },
+  {
+    id: 4,
+    image: cocktail,
+    name: "Fruit Cocktail",
+    numberOfOrders: 120,
+  },
+  {
+    id: 5,
+    image: spar,
+    name: "Sparkling Drink",
+    numberOfOrders: 100,
+  },
+  {
+    id: 6,
+    image: kafe_dosko,
+    name: "Kafe Dos Ko",
+    numberOfOrders: 90,
+  },
+];
